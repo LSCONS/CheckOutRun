71b2b6cfef3e2ef2615c59525b615bf2aa5d576a
jsw981117 <jsw981117@naver.com>
2025-02-24 16:28:22 +0900

    [feat] update ResultUI

--- Assets/02 Scripts/UI/ResultUI.cs
+++ Assets/02 Scripts/UI/ResultUI.cs
@@ -6,15 +6,38 @@ using UnityEngine;
 
 public class ResultUI : MonoBehaviour
 {
+    public TextMeshProUGUI resultTitle;
     public TextMeshProUGUI timeTxt;
     public TextMeshProUGUI scoreTxt;
 
-    public void UpdateTimeUI()
+    private void Start()
+    {
+        if (GameManager.Instance != null)
+        {
+            if (GameManager.Instance.isWin = true)
+            {
+                resultTitle.text = "퇴실 완료!";
+            }
+            else
+            {
+                resultTitle.text = "퇴실 실패...";
+            }
+        }
+        SetTimeTxt();
+        SetScoreTxt();
+    }
+
+    public void SetTimeTxt()
     {
         float gameTime = TimeManager.Instance.gameTime;
 
         int hours = Mathf.FloorToInt(gameTime / 60);
         int minutes = Mathf.FloorToInt(gameTime % 60);
         timeTxt.text = string.Format("{0:D2}:{1:D2}", hours, minutes);
+    }
+
+    public void SetScoreTxt()
+    {
+        scoreTxt.text = DataManager.Instance.Score.ToString();
     }
 }
