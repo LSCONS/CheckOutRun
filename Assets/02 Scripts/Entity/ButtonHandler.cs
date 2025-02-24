a13e714a9251acfc3457d22503c93ea00e0c0275
KJane0120 <rlawoals5755@gmail.com>
2025-02-24 15:58:50 +0900

    [Feat]일시정지 내 버튼 로직 구현
    일시정지 눌렀을때 팝업SetActive= true,
    Continue 클릭시 게임재개, Exit 클릭시 메인화면로드 함수 구현했습니다.

--- Assets/02 Scripts/Entity/ButtonHandler.cs
+++ Assets/02 Scripts/Entity/ButtonHandler.cs
@@ -3,38 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
     [SerializeField] private Player player;
     [SerializeField] private PlayerController playerController;
-
-
+    public Button pauseButton;
+    public GameObject pausePopUp;
     private void Start()
     {
         if (player == null)
         {
-            Debug.LogError("Player�� �Ҵ���� �ʾҽ��ϴ�.");
+            Debug.LogError("Player이 할당되지 않았습니다.");
             return;
         }
     }
 
-    public void LoadGamePage() //������ �����߽��ϴ�. + ResultScene���� �ٽ��ϱ� Ŭ���� �� �Լ� ���
+    public void LoadGamePage() //결과씬에서 다시하기 버튼 클릭시 이 함수 사용
     {
         SceneManager.LoadScene("GameScene");
     }
 
-    //public void LoadTutorialPage() Ʃ�丮�� ���� ��ȹ �� ���� ����
+    //public void LoadTutorialPage() //튜토리얼 구현시 사용
     //{
     //    SceneManager.LoadScene("TutorialScene")
     //}
 
-    public void LoadResultPage() //GameScene���� ���ӿ����ɶ� ��� �Լ�
+    public void LoadResultPage() //GameScene에서 GameOver시 사용
     {
         SceneManager.LoadScene("ResultScene");
     }
 
-    public void LoadMainPage() //ResultScene���� �ٽ��ϱ�/����ȭ�� �� ����ȭ�� Ŭ���� ��� �Լ�
+    public void LoadMainPage() //ResultScene에서 나가기or 메인화면으로 돌아가기 버튼 클릭시 이 함수 사용 
     {
         SceneManager.LoadScene("MainScene");
     }
@@ -55,6 +56,26 @@ public class ButtonHandler : MonoBehaviour
     {
         playerController.isSlide = false; 
         playerController.HandleSlide();
+    }
+
+    public void OpenPuasePopUpPage()
+    {
+        Time.timeScale = 0f;
+        pauseButton = GetComponentInChildren<Button>(true);
+        pausePopUp = GetComponentInChildren<GameObject>(true);
+        pausePopUp.SetActive(true);
+    }
+
+    public void OnClickContinue()
+    {
+        pausePopUp.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OnClickExit()
+    {
+        //pausePopUp.SetActive(false);
+        LoadMainPage();
     }
 
 }
