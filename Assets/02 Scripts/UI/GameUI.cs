cf58aa8c5e2d04ee6d4cbb59a54624e14459c1cd
KJane0120 <rlawoals5755@gmail.com>
2025-02-24 11:59:50 +0900

    [Feat]시간 흐르게 설정
    TimeManager 구현,
    GameUI에서 UpdateTimeUI에서 호출,
    실행을 하기 위해
    +Player rigidbody2D, box Collider2D, Player.cs, PlayerController.cs 추가
    +빈오브젝트 생성 후 TimeManager 추가

--- Assets/02 Scripts/UI/GameUI.cs
+++ Assets/02 Scripts/UI/GameUI.cs
@@ -8,12 +8,22 @@ public class GameUI : MonoBehaviour
     Player player;
     public RectTransform front;
     public TextMeshProUGUI scoreTxt;
+    [SerializeField] private TextMeshProUGUI Time;
 
     public void Start()
     {
         player = FindObjectOfType<Player>();
+        if (Time == null)
+        {
+            Debug.LogError("Time is not assigned!");
+            return;
+        }
     }
 
+    private void Update()
+    {
+        UpdateTimeUI();
+    }
     public void UpdateHealthBar() // hp가 변경될 때마다 호출해서 UI를 업데이트해줘야함
     {
         float hp = player.playerHealth / 100f;
@@ -28,6 +38,10 @@ public class GameUI : MonoBehaviour
 
     public void UpdateTimeUI()
     {
-
+        float gameTime = TimeManager.Instance.gameTime;
+        
+        int hours = Mathf.FloorToInt(gameTime / 60);
+        int minutes = Mathf.FloorToInt(gameTime % 60);
+        Time.text = string.Format("{0:D2}:{1:D2}", hours, minutes);
     }
 }
