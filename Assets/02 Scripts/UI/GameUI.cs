4566a5e65a5792176fd7c47d01ec82b158518ee8
jsw981117 <jsw981117@naver.com>
2025-02-24 10:16:56 +0900

    [feat] add GameUI

--- Assets/02 Scripts/UI/GameUI.cs
+++ Assets/02 Scripts/UI/GameUI.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
-    // Player가 GameUI를 가지고 있게 하면 될듯
+    Player player;
     public RectTransform front;
+    public TextMeshProUGUI scoreTxt;
 
-    //public void UpdateHealthBar() // hp가 변경될 때마다 호출해서 UI를 업데이트해줘야함
-    //{
-    //    front.localScale = new Vector3(hp, 1f, 1f);
-    //}
+    public void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
 
+    public void UpdateHealthBar() // hp가 변경될 때마다 호출해서 UI를 업데이트해줘야함
+    {
+        float hp = player.playerHealth / 100f;
+        front.localScale = new Vector3(hp , 1f, 1f);
+    }
 
+    public void UpdateScoreUI()
+    {
+        int score = DataManager.Instance.Score;
+        scoreTxt.text = score.ToString();
+    }
+
+    public void UpdateTimeUI()
+    {
+
+    }
 }
