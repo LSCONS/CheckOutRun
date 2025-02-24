86bb120c5bc93cb4ea6a6c7552589c801cc2d034
LSCONS <songjeu26@naver.com>
2025-02-24 15:20:09 +0900

    [Feat]바닥 자동 생성 로직 구현

--- Assets/02 Scripts/InstancePrefabs/InstancePrefabs.cs
+++ Assets/02 Scripts/InstancePrefabs/InstancePrefabs.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class InstancePrefabs : MonoBehaviour
 {
     public GameObject[] Platforms;
+    [SerializeField] private int instanceNum = 40;
+    [SerializeField] private int startPositionX = -3;
+    [SerializeField] private int startPositionY = -1;
+    [SerializeField] private int width = 5;
+    [SerializeField] private float minusY = 0.4f;
+
+
+    private void Awake()
+    {
+        for(int i = 0; i < instanceNum; i++)
+        {
+            int rand = Random.Range(0, Platforms.Length);
+            GameObject instacne = Instantiate(Platforms[rand], new Vector3(startPositionX + (i * width), startPositionY, 0), Quaternion.identity);
+            BoxCollider2D box = instacne.AddComponent<BoxCollider2D>();
+            box.size = new Vector2(box.size.x, box.size.y - (minusY / instacne.transform.localScale.y));
+            box.offset = new Vector2(box.offset.x, box.offset.y - ((minusY/ 2) / instacne.transform.localScale.y));
+        }
+    }
 }
