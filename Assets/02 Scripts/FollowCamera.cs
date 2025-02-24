e3cfdc748b7791cfb451514cf8b834380f4d7f8b
KJane0120 <rlawoals5755@gmail.com>
2025-02-24 20:30:04 +0900

    [Feat]배경 플레이어 이동속도 0.5배속 추적
    FollowCamera에서 배경을 플레이어 이동속도의 0.5배속으로 따라가게 만들도록 구현했습니다.
    배경 이미지 간단하게 구현해봤습니다.
    +추후 작업을 위해 Layer BackGround 추가

--- Assets/02 Scripts/FollowCamera.cs
+++ Assets/02 Scripts/FollowCamera.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public Transform target;
-    float offsetX; // ī�޶� �ʱ� x��ġ
-    
+    public Transform backGround;
+    //float offsetX; // ī�޶� �ʱ� x��ġ
+
+    [SerializeField] private float followSpeed = 5f;
+    [SerializeField] private float parallaxFactor = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        offsetX = transform.position.x - target.position.x;
+        //offsetX = transform.position.x - target.position.x;
     }
 
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+    //    Vector3 pos = transform.position;
+    //    pos.x = target.position.x + offsetX;
+    //    transform.position = pos;
+    //}
+
+    private void LateUpdate()
     {
-        Vector3 pos = transform.position;
-        pos.x = target.position.x + offsetX;
-        transform.position = pos;
+        if( target == null) return;
+
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+
+        if(backGround != null)
+        {
+            backGround.position = new Vector3(transform.position.x * parallaxFactor, backGround.position.y, 0f); 
+        }
     }
 }
