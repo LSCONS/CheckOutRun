d5c0fbec97789a4ea1f93602d9a5776c424d52f3
hee103 <gmlgml3213@gmail.com>
2025-02-24 10:32:41 +0900

    [Refactor] Player 스크립트 개선
    - PlayerController 스크립트에 플레이어 동작을 제어할 수 있게 구현
    - Palyer 스크립트와 PlayerController 연결

--- Assets/02 Scripts/Player/PlayerController.cs
+++ Assets/02 Scripts/Player/PlayerController.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Player player;
+
     void Start()
     {
-        
+        player = GetComponent<Player>();
+
+        if (player == null)
+        {
+            Debug.LogError("PlayerController: Player 컴포넌트를 찾을 수 없습니다!");
+            return;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player == null) return;
+
+        HandleJump();
+        HandleSlide();
+    }
+
+    void FixedUpdate()
+    {
+        if (player == null) return;
+
+        Move();
+    }
+
+    private void Move()
+    {
+        if (player.rigid != null)
+        {
+            player.rigid.velocity = new Vector2(player.playerSpeed, player.rigid.velocity.y);
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (player.jumpCount < player.maxJumpCount)
+            {
+                Jump();
+                player.jumpCount++;
+            }
+        }
+        if (player.rigid.velocity.y == 0)
+        {
+            player.jumpCount = 0;
+        }
+    }
+
+    private void Jump()
+    {
+        if (player.rigid != null)
+        {
+            player.rigid.velocity = new Vector2(player.rigid.velocity.x, player.jumpForce);
+        }
+    }
+
+    private void HandleSlide()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Slide();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            ResetSlide();
+        }
+    }
+
+    private void Slide()
+    {
+        if (player.coll != null)
+        {
+            player.coll.size = new Vector2(player.originalColliderSize.x, player.originalColliderSize.y * 0.5f);
+        }
+    }
+
+    private void ResetSlide()
+    {
+        if (player.coll != null)
+        {
+            player.coll.size = player.originalColliderSize;
+        }
     }
 }
+
+
