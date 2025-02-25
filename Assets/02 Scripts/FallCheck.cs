847c49c358272ec0962f9731311402a5673be09a
KJane0120 <rlawoals5755@gmail.com>
2025-02-25 15:34:01 +0900

    [Feat]플레이어 추락시 게임오버 처리
    낭떠러지 구간 밑에 Collision 생성 후 OnTriggerEnter2D를 통해 게임오버 로직 구현했습니다.

---
+++ Assets/02 Scripts/FallCheck.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FallCheck : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == null) return;
+
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("플레이어는 죽었어요");
+            GameManager.Instance.isWin = false;
+            GameManager.Instance.GameOver();
+        }
+    }
+}
