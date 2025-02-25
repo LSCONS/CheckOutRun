2048938810831e359391f6d3a648d87d0836e504
KJane0120 <rlawoals5755@gmail.com>
2025-02-25 14:23:27 +0900

    [Fix]시간 수정
    맵 길이 * 3이라 시간 * 3을 지웠습니다.
    21시가 넘을 시 21:00로 고정시키는 걸로 수정했습니다.
    정상작동 확인했습니다.

--- Assets/02 Scripts/Manager/TimeManager.cs
+++ Assets/02 Scripts/Manager/TimeManager.cs
@@ -28,32 +28,16 @@ public class TimeManager : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        //속도 증가 아이템을 먹었다면 gameTime에 speedMultipler을 곱해야 함
-        //24시간 기준으로 시간 제한
-        if(gameTime >= endHour)
-        {
-            //바로 다음날 9시로 넘어가는게 아니라, 보스전동안 잠시 대기하는 조건문 필요
-            //if(isBoss == true)
-            //{
-
-            //}
-            //else
-            gameTime = startHour;
-        }
-    }
-
     public void AddGameTime(float moveDistance)
     {
-        float TimetoAdd = moveDistance * 3f; // 플레이어 이동거리에 따라 더해주는거라 Time.deltaTime을 곱해주지 않아도 됨
+        float TimetoAdd = moveDistance; // 플레이어 이동거리에 따라 더해주는거라 Time.deltaTime을 곱해주지 않아도 됨
         gameTime = TimetoAdd + startHour;
 
         if (gameTime >= endHour)
         {
             //보스전 동안 moveDistance를 카운트하면 안됨
 
-            gameTime = startHour;
+            gameTime = endHour;
         }
     }
 }
