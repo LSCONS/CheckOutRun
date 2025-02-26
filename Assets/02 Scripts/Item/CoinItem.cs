4bce43c50afc3abcfc6b8e874a82627ad16efd50
jsw981117 <jsw981117@naver.com>
2025-02-26 19:29:28 +0900

    [feat] add pickupCoinSFX

--- Assets/02 Scripts/Item/CoinItem.cs
+++ Assets/02 Scripts/Item/CoinItem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CoinItem : MonoBehaviour, IItem
 {
     private int coinScore = 1;          //충돌시 올라가는 점수 수치
+    public AudioClip pickupCoinSFX;
 
     public int CoinScore { get { return coinScore; } }
 
@@ -16,9 +17,13 @@ public class CoinItem : MonoBehaviour, IItem
     private void OnTriggerEnter2D(Collider2D collision)
     {
        if(collision.CompareTag("Player"))
-        {
+       {
+            if (SoundManager.Instance != null)
+            {
+                SoundManager.Instance.sfxManager.PlaySFX(pickupCoinSFX, 0.2f);
+            }
             DataManager.Instance.AddScore(CoinScore);
             OnCollisionEffect();
-        }              
+       }
     }
 }
