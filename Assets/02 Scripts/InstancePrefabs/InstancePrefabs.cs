0983c749ae270d70f45d8c8062de605ca1a0f697
LSCONS <songjeu26@naver.com>
2025-02-26 17:14:03 +0900

    [Feat] 배경 색깔 조정

--- Assets/02 Scripts/InstancePrefabs/InstancePrefabs.cs
+++ Assets/02 Scripts/InstancePrefabs/InstancePrefabs.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InstancePrefabs : MonoBehaviour
@@ -10,12 +12,22 @@ public class InstancePrefabs : MonoBehaviour
     private float minRandHeightY = 0;
     private float minRandWidthX = 3;
     private float maxRandWidthX = 7;
+    private SpriteRenderer[] backgroundsSpriteRenderer;
+    private Color minusColor = new Color(0.1f, 0.1f, 0.1f, 0f);
+    private GameObject player;
+    [SerializeField]private Color beforeColor;
+    [SerializeField]private Color nextColor;
+    float playerPositionX;
+    private bool isHourTime = false;
+    private float nowTime = 60f;
+    private float lerpT = 0;
 
 
     private void Awake()
     {
+        player = GameObject.Find("Player");
         float nowWidthX = 0;
-        while(nowWidthX < maxWidthX)
+        while (nowWidthX < maxWidthX)
         {
             int randIndex = Random.Range(0, backgroundPrefabs.Length);
             float randHeightY = Random.Range(minRandHeightY, maxRandHeightY);
@@ -25,7 +37,7 @@ public class InstancePrefabs : MonoBehaviour
             gameObject.transform.parent = this.transform;
 
             Renderer renderer = gameObject.GetComponent<Renderer>();
-            if(renderer != null)
+            if (renderer != null)
             {
                 nowWidthX += renderer.bounds.size.x + randWidthX;
                 Color beforeColor = renderer.material.color;
@@ -37,4 +49,48 @@ public class InstancePrefabs : MonoBehaviour
             }
         }
     }
+
+    private void Start()
+    {
+        backgroundsSpriteRenderer = GetComponentsInChildren<SpriteRenderer>();
+        beforeColor = new Color(0.6f, 0.6f, 0.6f, 1);
+        nextColor = new Color(0.6f, 0.6f, 0.6f, 1);
+        for (int i = 0; i < backgroundsSpriteRenderer.Length; i++)
+        {
+            backgroundsSpriteRenderer[i].color = beforeColor;
+        }
+    }
+
+
+    private void Update()
+    {
+        Debug.Log("playerPositionX = " + player.transform.position.x);
+        if(nowTime <= player.transform.position.x)
+        {
+            if(nowTime < 299)
+            {
+                nextColor = nextColor + minusColor;
+            }
+            else
+            {
+                nextColor = nextColor - minusColor;
+            }
+            lerpT = 0;
+            nowTime += 60f;
+            Debug.Log("before = " + beforeColor);
+            Debug.Log("next = " + nextColor);
+        }
+
+        if(beforeColor != nextColor)
+        {
+            lerpT += Time.deltaTime * 2;
+            if (lerpT >= 1) lerpT = 1;
+            beforeColor = Color.Lerp(beforeColor, nextColor, lerpT);
+            for (int i = 0; i < backgroundsSpriteRenderer.Length; i++)
+            {
+                backgroundsSpriteRenderer[i].color = beforeColor;
+            }
+        }
+    }
+
 }
--- Assets/02 Scripts/Player/Player.cs
+++ Assets/02 Scripts/Player/Player.cs
@@ -57,7 +57,6 @@ public class Player : MonoBehaviour
         {
             yield return new WaitForSeconds(1f);
             statHandler.Damage(1);
-            Debug.Log(playerHealth);
 
             if (playerHealth <= 0) break;
         }
