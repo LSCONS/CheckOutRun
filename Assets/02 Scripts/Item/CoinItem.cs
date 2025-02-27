851f65b61fbfb1a5bd3a95fd957b427bf935471e
hee103 <gmlgml3213@gmail.com>
2025-02-27 12:00:16 +0900

    충돌로 코드 수정

--- Assets/02 Scripts/Item/CoinItem.cs
+++ Assets/02 Scripts/Item/CoinItem.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class CoinItem : MonoBehaviour, IItem
 {
-    private int baseScore = 1;       // 기본 점수
-    private int EventScore = 2;    // 강화된 점수
-    private static bool isBoosted = false; // 점수 강화 상태 여부
+    private int coinScore = 1;          //충돌시 올라가는 점수 수치
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] ParticleSystem particle;
 
-    public int CoinScore { get { return isBoosted ? EventScore : baseScore; } } // 강화 상태면 boostedScore 반환
+    public int CoinScore { get { return coinScore; } }
 
     public void OnCollisionEffect()
     {
-        Destroy(gameObject);
-        // 애니메이션 동작 후 삭제 로직 필요
+        if (spriteRenderer == null)
+        {
+            Destroy(gameObject, 1f);
+        }
+        else
+        {
+            particle.Play();
+            Destroy(spriteRenderer.gameObject);
+            Destroy(gameObject, 1f);
+        }
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -24,22 +31,8 @@ public class CoinItem : MonoBehaviour, IItem
             {
                 SoundManager.Instance.sfxManager.PlaySFX(SoundLibrary.Instance.sfxPickupCoin, 0.2f);
             }
-            DataManager.Instance.AddScore(CoinScore); // 현재 상태에 따라 점수 추가
+            DataManager.Instance.AddScore(CoinScore);
             OnCollisionEffect();
         }
-    }
-
-    public static void ActivateCoinEvent(float duration)
-    {
-        isBoosted = true;
-        Debug.Log("코인 점수 상승! " + duration + "초 동안 유지됨.");
-        GameManager.Instance.StartCoroutine(DeactivateEventAfterTime(duration));
-    }
-
-    private static IEnumerator DeactivateEventAfterTime(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        isBoosted = false;
-        Debug.Log("코인 점수 원래대로 복귀.");
     }
 }
