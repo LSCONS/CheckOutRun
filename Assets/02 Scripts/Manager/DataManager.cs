2623aeeea02632e9b31b2aba5fc59c9b5514b792
ProgramCnt <corfafa2@naver.com>
2025-02-28 11:24:37 +0900

    [Style] 요약 형식으로 변경

--- Assets/02 Scripts/Manager/DataManager.cs
+++ Assets/02 Scripts/Manager/DataManager.cs
@@ -24,21 +24,28 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    //점수 데이터부분 초기화 작업
+    /// <summary>
+    /// 점수 데이터 부분 초기화 작업
+    /// </summary>
     public void Init()
     {
         score = 0;
         gameUI = FindObjectOfType<GameUI>();
     }
 
-    //점수 더하기
+    /// <summary>
+    /// 점수 더하기
+    /// </summary>
+    /// <param name="unitScore">추가할 점수</param>
     public void AddScore(int unitScore)
     {
         score += unitScore;
         gameUI.UpdateScoreUI();
     }
 
-    //최고기록 갱신
+    /// <summary>
+    /// 최고 기록 갱신
+    /// </summary>
     public void UpdateBestScore()
     {
         int bestScore = PlayerPrefs.GetInt("bestScore");
